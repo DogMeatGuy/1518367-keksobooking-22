b943652aaccdb371b635780557c7668b4971cefa
DogMeatGuy <70135714+DogMeatGuy@users.noreply.github.com>
2021-02-21 13:18:46 +0300

    Первое задание(я поборол!)

--- js/main.js
+++ js/main.js
@@ -10,7 +10,13 @@ function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+//Функция возвращающая случайное дробное число из заданных параметров включительно. Количество знаков после запятой = 1.
 
-function getRandomFloat(min, max) {
-  return Math.random() * (max - min) + min;
+let randomFloat = function getRandomFloat(min, max) {
+  let errorMessage = ('Не верное значение, попробуйте снова');
+  if (max <= min) {
+   return errorMessage;
+  }
+  let randomFloat = Math.random() * (max - min + 1) + min;
+ return randomFloat.toFixed(1);
 }
